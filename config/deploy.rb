77b7592c67e8b372bc8cca95e8d9252cafaa0466
Bryan Baker <bryanpbaker@gmail.com>
2016-04-11 21:07:32 -0500

    Added cap for deployment

---
+++ config/deploy.rb
@@ -0,0 +1,8 @@
+# config valid only for Capistrano 3.1
+lock '>=3.2.1'
+
+set :application, 'superhero-quiz'
+set :repo_url, 'git@github.com:bekahlbaker/superhero-quiz.git'
+
+# set :linked_dirs, %w{wp-content/uploads wp-content/plugins/all-in-one-wp-migration/storage}
+# set :linked_files, %w{wp-config.php}
