0300444397cc92fddd41ab98024f98dd2ce7b048
Bekah Baker <bekahlbaker@gmail.com>
2016-04-19 14:51:21 -0500

    booyah - made enter work on correct answer

--- js/main.js
+++ js/main.js
@@ -18,12 +18,11 @@ $(document).ready(function(){
 $('.herobox').click(function() {	
 
 	var clickedOne = $(this);
-		
+
     $(clickedOne).find('#textArea').focus();
 
   		  
-  	});		  
-
+});		  
 
 	var correctAnswers = 0;
 	var levelZero = 'ignorant';
@@ -33,12 +32,12 @@ $('.herobox').click(function() {
 	var levelFour = 'advanced';
 	var levelFive = 'master';
 
+
 $('input[name="guess"]').keyup(function(){
     this.value = this.value.toLowerCase();
 });
 
-
-	$('.submit').click(function() {
+$('.submit').click(function() {
 
 
 		var clickedOne = $(this);
@@ -48,11 +47,12 @@ $('input[name="guess"]').keyup(function(){
 				$(clickedOne).siblings('.guess1').val() === 'flash') {
 
 			$(clickedOne).siblings('.guess1').css('background-color', '#73AF59');
-			correctAnswers++;
+			  previousValue = correctAnswers;
+			 	correctAnswers++;
 			$(clickedOne).hide();
 	        $(clickedOne).siblings('.guess1').prop('disabled', true);
 
-	        // $(this).next().find('input[name="guess"]').focus();
+
 
 		} else {
 			$(clickedOne).siblings('.guess1').css('background-color', '#D72729');
@@ -62,9 +62,11 @@ $('input[name="guess"]').keyup(function(){
 				$(clickedOne).siblings('.guess2').val() === 'ant man') {
 
 			$(clickedOne).siblings('.guess2').css('background-color', '#73AF59');
-			correctAnswers++;
+			 previousValue = correctAnswers;
+			 	correctAnswers++;
 			$(clickedOne).hide();
 	        $(clickedOne).siblings('.guess2').prop('disabled', true);
+
 
 		} else {
 			$(clickedOne).siblings('.guess2').css('background-color', '#D72729');
@@ -74,7 +76,8 @@ $('input[name="guess"]').keyup(function(){
 				$(clickedOne).siblings('.guess3').val() === 'dead pool') {
 
 			$(clickedOne).siblings('.guess3').css('background-color', '#73AF59');
-			correctAnswers++;
+			 previousValue = correctAnswers;
+			 	correctAnswers++;
 			$(clickedOne).hide();
 	        $(clickedOne).siblings('.guess3').prop('disabled', true);
 
@@ -83,39 +86,55 @@ $('input[name="guess"]').keyup(function(){
 		}
 
 
-	$('#score').html(correctAnswers+'/33');
+		$('#score').html(correctAnswers+'/33');
 
-	if (correctAnswers == 0) {
-		$('#level').html(levelZero);
-	} else if ((correctAnswers >=1) && (correctAnswers <=6)) {
-		$('#level').html(levelOne);
-	} else if ((correctAnswers >=7) && (correctAnswers <=13)) {
-		$('#level').html(levelTwo);
-	} else if ((correctAnswers >=14) && (correctAnswers <=20)) {
-		$('#level').html(levelThree);
-	} else if ((correctAnswers >=21) && (correctAnswers <=27)) {
-		$('#level').html(levelFour);
-	} else if ((correctAnswers >=28) && (correctAnswers <=33)){
-		$('#level').html(levelFive);
-	} else if (correctAnswers == 33) {
-		$('#surprise').html("<img class= 'surprise' src='http://38.media.tumblr.com/a2cc17ac37aa28733928286fe62e0df6/tumblr_mla8voaODR1ra9eteo1_1280.gif'/>");
-	}
+		if (correctAnswers == 0) {
+			$('#level').html(levelZero);
+		} else if ((correctAnswers >=1) && (correctAnswers <=6)) {
+			$('#level').html(levelOne);
+		} else if ((correctAnswers >=7) && (correctAnswers <=13)) {
+			$('#level').html(levelTwo);
+		} else if ((correctAnswers >=14) && (correctAnswers <=20)) {
+			$('#level').html(levelThree);
+		} else if ((correctAnswers >=21) && (correctAnswers <=27)) {
+			$('#level').html(levelFour);
+		} else if ((correctAnswers >=28) && (correctAnswers <=33)){
+			$('#level').html(levelFive);
+		} else if (correctAnswers == 33) {
+			$('#surprise').html("<img class= 'surprise' src='http://38.media.tumblr.com/a2cc17ac37aa28733928286fe62e0df6/tumblr_mla8voaODR1ra9eteo1_1280.gif'/>");
+		}
 
-	});
-
-
-
-	$('#reset').click(function(){
-	location.reload(true)
 });
 
 
 $('input[name="guess"]').keyup(function (e) {
-	var submit = $(this).siblings('.submit');
+		var submit = $(this).siblings('.submit');
 
-    if (e.keyCode == 13) {
-   		$(submit).click();
-    }
+
+	    if (e.keyCode == 13) {
+	   		$(submit).click();
+
+	   		console.log(previousValue);
+	   		console.log(correctAnswers);
+
+		   		textboxes = $('input[name="guess"]');
+		        currentBoxNumber = textboxes.index(this);
+		        if (correctAnswers != previousValue) {
+		            nextBox = textboxes[currentBoxNumber + 1]
+		            nextBox.focus();
+		            nextBox.select();
+		            // event.preventDefault();
+		            // return false 
+		            previousValue++;
+		            }
+	    }
+
+
+});
+
+
+$('#reset').click(function(){
+	location.reload(true)
 });
 
 
