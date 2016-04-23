5395bbda764298e3de1a478646abaa067531b109
Bryan Baker <bryanpbaker@gmail.com>
2016-04-23 11:38:42 -0500

    updated cap

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 server '104.131.38.234', user: "deploy", roles: %w{app}
 
 set :branch, "master"
-set :deploy_to, "/var/www/dc-marvel-quiz/html"
+set :deploy_to, "/var/www/comic-quiz.com/html"
 
 set :ssh_options, {
   forward_agent: true,
